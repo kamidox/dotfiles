b92aba2027412d1b276cbcbaf9b95cdcd4a2b7d1
Joey.Huang <kamidox@qq.com>
2018-06-02 17:46:34 +0800

    ipython config

---
+++ .ipython/profile_default/ipython_config.py
@@ -0,0 +1,7 @@
+c = get_config()
+# Restore Magics automatically
+c.StoreMagics.autorestore = True
+# Run all nodes interactively
+c.InteractiveShell.ast_node_interactivity = 'all'
+c.InlineBackend.figure_format = 'retina'
+
